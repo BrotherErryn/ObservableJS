d3ff3ae3a87347574e78b431208046a6f0110bb9
Erryn Pollock <brothererryn@atomicmonks.com>
2013-09-03 16:46:01 -0500

    Create example.js
    Example usage of ObservableJS.

---
+++ example.js
@@ -0,0 +1,42 @@
+window.myViewModel = {
+	name: "Bob",
+	nameIsAwesome: true,
+	items: [
+		{ name:"Sandwich", amount: 2.25 },
+		{ name:"Fries", amount: 1.00 },
+		{ name:"Soda", amount: 1.55 },
+		{ name:"Shards of Glass", amount: 0.10 },
+		{ name:"Screwdriver", amount: 5.00 }
+	],
+	selectedItem : null,
+	selectedItems : [],
+	selectionText : ""
+};
+
+Observable.On(); // turn on connection to DOM
+
+// some late binding...
+Observable.Watch({
+	model: myViewModel,
+	property: "nameIsAwesome",
+	callback: function(o, v) {
+		console.log( v ? "AWESOME" : "NOT AWESOME");
+	}
+});
+Observable.Watch({
+	model: myViewModel,
+	property: "selectedItems",
+	callback: function(o, v) {
+		console.log("Generating selected items message");
+		var message = "";
+		if(myViewModel.selectedItems.length) {
+			var names = [];
+			for(var i=0;i<myViewModel.selectedItems.length;i++) {
+				names.push(myViewModel.selectedItems[i].name);
+			}
+			message = names.join(", ");
+			message = "You selected: " + message;
+		}
+		myViewModel.selectionText = message;
+	}
+});
